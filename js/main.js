e4ec5c26ba877410bd62368a3a24dda6d65e6742
mlizon <122919166+mlizon@users.noreply.github.com>
2023-03-05 17:13:15 +0100

    obsługa formularzy

---
+++ js/main.js
@@ -0,0 +1,15 @@
+console.log(`Warsztat- obsługa formularza "Imie i nazwisko"`);
+
+const submitForm = (event) => {
+    event.preventDefault();
+
+    let firstName = document.querySelector('[name="fname"]');
+    let lastName = document.querySelector(`[name="lname"]`);
+
+    console.log(`Imie: ${firstName.value}, Nazwisko: ${lastName.value}`);
+}
+
+let form = document.getElementById('form');
+
+form.addEventListener('submit', submitForm);
+
